4533e6c5572375c5aad67c1824f579a92d117028
calincrist <calin.crist@gmail.com>
2015-01-12 23:29:53 +0200

    Inital commit

---
+++ production.js
@@ -0,0 +1,19 @@
+// production.js
+var deployd = require('deployd');
+
+var server = deployd({
+  port: 5001
+});
+
+server.listen();
+
+server.on('listening', function() {
+  console.log("Server is listening");
+});
+
+server.on('error', function(err) {
+  console.error(err);
+  process.nextTick(function() { // Give the server a chance to return an error
+    process.exit();
+  });
+});
